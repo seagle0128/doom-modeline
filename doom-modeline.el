7665721db1a39111744f2ee6d2e1abb272c605f8
Vincent Zhang <seagle0128@gmail.com>
2018-07-05 13:56:23 +0800

    Update README and package descriptions.

--- doom-modeline.el
+++ doom-modeline.el
@@ -31,7 +31,6 @@
 ;;
 ;; This package offers a modern modeline them which is extraced from DOOM Emacs
 ;; (https://github.com/hlissner/doom-emacs/tree/master/modules/ui/doom-modeline).
-;;
 ;; It's also the part of Centaur Emacs (https://github.com/seagle0128/.emacs.d).
 ;;
 ;; The DOOM modeline was designed for minimalism, and offers:
@@ -41,6 +40,16 @@
 ;; 4. A customizable mode-line height (see doom-modeline-height)
 ;; 5. An error/warning count segment for flycheck
 ;;
+;; Installation:
+;; From melpa, `M-x package-install RET doom-modeline RET`.
+;; In `init.el`,
+;; (require 'doom-modeline)
+;; (doom-modeline-init)
+;; or
+;; (use-package doom-modeline
+;;   :ensure t
+;;   :defer t
+;;   :hook (after-init . doom-modeline-init))
 
 ;;; Code:
 
