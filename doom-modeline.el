7607cc5c8149ecdbede1fa3a494dea6dcb74465d
Vincent Zhang <seagle0128@gmail.com>
2018-07-05 16:39:41 +0800

    Use Homepage instead of URL.

--- doom-modeline.el
+++ doom-modeline.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2018 Vincent Zhang
 
 ;; Author: Vincent Zhang <seagle0128@gmail.com>
-;; URL: https://github.com/seagle0128/doom-modeline
+;; Homepage: https://github.com/seagle0128/doom-modeline
 ;; Version: 0.2.0
 ;; Package-Requires: ((emacs "25.1") (all-the-icons "1.0.0") (projectile "0.10.0") (shrink-path "0.2.0") (eldoc-eval "0.1") (dash "2.11.0"))
 ;; Keywords: faces
