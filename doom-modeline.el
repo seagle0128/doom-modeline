c89048b52912c9635fff96a8cc65d0bb266d6ba8
Vincent Zhang <seagle0128@gmail.com>
2018-08-13 11:01:08 +0800

    Only enable process segment in the active window.

--- doom-modeline.el
+++ doom-modeline.el
@@ -628,7 +628,7 @@ directory, the file name, and its state (modified, read-only or non-existent)."
 
 (doom-modeline-def-segment process
   "The process info."
-  (if mode-line-process
+  (if (and (doom-modeline--active) mode-line-process)
       mode-line-process
     ""))
 
