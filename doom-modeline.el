bc06fadfc7cbdcbd5c31adc7cae0d4769de284d4
Vincent Zhang <seagle0128@gmail.com>
2018-09-11 00:38:31 +0800

    Update package description.

--- doom-modeline.el
+++ doom-modeline.el
@@ -1,4 +1,4 @@
-;;; doom-modeline.el --- A minimal modeline from DOOM.  -*- lexical-binding: t; -*-
+;;; doom-modeline.el --- A minimal and modern modeline -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2018 Vincent Zhang
 
