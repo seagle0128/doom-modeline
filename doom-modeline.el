8cf0646dc0da1af769384b6f5b927349d6cc3f9a
Vincent Zhang <seagle0128@gmail.com>
2018-11-15 14:13:50 +0800

    Remove unnecessary git-timemachine-mode-hook.

--- doom-modeline.el
+++ doom-modeline.el
@@ -627,7 +627,6 @@ buffer where knowing the current project directory is important."
 (add-hook 'find-file-hook #'doom-modeline-update-buffer-file-name)
 (add-hook 'after-revert-hook #'doom-modeline-update-buffer-file-name)
 (add-hook 'after-save-hook #'doom-modeline-update-buffer-file-name)
-(add-hook 'git-timemachine-mode-hook #'doom-modeline-update-buffer-file-name)
 (advice-add #'rename-buffer :after #'doom-modeline-update-buffer-file-name)
 
 (doom-modeline-def-segment buffer-info
