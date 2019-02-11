5632bb02b59b2e5b176cbfcf57da34b45b2e340c
Vincent Zhang <seagle0128@gmail.com>
2019-02-12 04:15:19 +0800

    Bump version to 1.7.1.

--- doom-modeline.el
+++ doom-modeline.el
@@ -4,7 +4,7 @@
 
 ;; Author: Vincent Zhang <seagle0128@gmail.com>
 ;; Homepage: https://github.com/seagle0128/doom-modeline
-;; Version: 1.7.0
+;; Version: 1.7.1
 ;; Package-Requires: ((emacs "25.1") (all-the-icons "1.0.0") (shrink-path "0.2.0") (eldoc-eval "0.1") (dash "2.11.0"))
 ;; Keywords: faces mode-line
 
