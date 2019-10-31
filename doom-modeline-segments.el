2cf48d76ae786769536988b4e2ce4412ff460b26
Vincent Zhang <seagle0128@gmail.com>
2019-10-31 16:57:48 +0800

    Fix #246: Incompatible with (setq vc-display-status nil).

--- doom-modeline-segments.el
+++ doom-modeline-segments.el
@@ -637,7 +637,9 @@ Uses `all-the-icons-octicon' to fetch the icon."
         (when (and vc-mode buffer-file-name)
           (let* ((backend (vc-backend buffer-file-name))
                  (state (vc-state buffer-file-name backend))
-                 (str (substring vc-mode (+ (if (eq backend 'Hg) 2 3) 2))))
+                 (str (if vc-display-status
+                          (substring vc-mode (+ (if (eq backend 'Hg) 2 3) 2))
+                        "")))
             (propertize (if (> (length str) doom-modeline-vcs-max-length)
                             (concat
                              (substring str 0 (- doom-modeline-vcs-max-length 3))
