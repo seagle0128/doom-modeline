bb4d748ed50fba4ddcf96491a99727e9e8e9a7e9
Vincent Zhang <seagle0128@gmail.com>
2019-12-08 01:17:31 +0800

    Bump version.

--- doom-modeline.el
+++ doom-modeline.el
@@ -4,7 +4,7 @@
 
 ;; Author: Vincent Zhang <seagle0128@gmail.com>
 ;; Homepage: https://github.com/seagle0128/doom-modeline
-;; Version: 2.8.1
+;; Version: 2.8.2
 ;; Package-Requires: ((emacs "25.1") (all-the-icons "1.0.0") (shrink-path "0.2.0") (dash "2.11.0"))
 ;; Keywords: faces mode-line
 
