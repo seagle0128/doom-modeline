8c71b43fee81f920f7acae64c8bfb978bc80562d
Vincent Zhang <seagle0128@gmail.com>
2020-04-06 23:36:28 +0800

    Refresh the mode-line of the current buffer while toggling.

--- doom-modeline.el
+++ doom-modeline.el
@@ -243,6 +243,7 @@ So it can be restored when `doom-modeline-mode' is disabled.")
   (if doom-modeline-mode
       (progn
         (doom-modeline-refresh-bars)        ; Create bars
+        (doom-modeline-set-main-modeline)   ; Set mode-line for current buffer
         (doom-modeline-set-main-modeline t) ; Set default mode-line
 
         ;; These buffers are already created and don't get modelines
@@ -272,6 +273,7 @@ So it can be restored when `doom-modeline-mode' is disabled.")
         (advice-add #'helm-display-mode-line :after #'doom-modeline-set-helm-modeline))
     (progn
       ;; Restore mode-line
+      (setq mode-line-format doom-modeline--default-format)
       (setq-default mode-line-format doom-modeline--default-format)
       (dolist (bname '("*scratch*" "*Messages*"))
         (if (buffer-live-p (get-buffer bname))
