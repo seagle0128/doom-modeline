cf1ac1be52d56160a372cd93656d3873b232fc56
Eric Dallo <ericdallo06@hotmail.com>
2020-05-28 14:21:45 -0300

    Change color for all buffers

--- doom-modeline-segments.el
+++ doom-modeline-segments.el
@@ -1960,18 +1960,24 @@ mouse-3: Fetch notifications"
 ;;
 
 ;; Highlight the mode-line while debugging.
-(defvar-local doom-modeline--debug-cookie nil)
+(defvar doom-modeline--debug-cookie nil)
 (defun doom-modeline--debug-visual (&rest _)
   "Update the face of mode-line for debugging."
-  (setq doom-modeline--debug-cookie
-        (face-remap-add-relative 'mode-line 'doom-modeline-debug-visual))
-  (force-mode-line-update))
+  (mapc (lambda (buffer)
+          (with-current-buffer buffer
+            (setq doom-modeline--debug-cookie
+                  (face-remap-add-relative 'mode-line 'doom-modeline-debug-visual))
+            (force-mode-line-update)))
+        (buffer-list)))
 
 (defun doom-modeline--normal-visual (&rest _)
   "Restore the face of mode-line."
-  (when doom-modeline--debug-cookie
-    (face-remap-remove-relative doom-modeline--debug-cookie)
-    (force-mode-line-update)))
+  (mapc (lambda (buffer)
+          (when doom-modeline--debug-cookie
+            (with-current-buffer buffer
+              (face-remap-remove-relative doom-modeline--debug-cookie)
+              (force-mode-line-update))))
+        (buffer-list)))
 
 (add-hook 'dap-session-created-hook #'doom-modeline--debug-visual)
 (add-hook 'dap-terminated-hook #'doom-modeline--normal-visual)
