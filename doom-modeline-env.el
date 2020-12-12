5032c7acf084209c1c60e58a8b847ec2108468c8
Vincent Zhang <seagle0128@gmail.com>
2020-12-13 02:23:29 +0800

    Format codes.

--- doom-modeline-env.el
+++ doom-modeline-env.el
@@ -196,17 +196,17 @@ PARSER should be a function for parsing COMMAND's output line-by-line, to
 (doom-modeline-def-env python
   :hooks   'python-mode-hook
   :command (lambda () (cond ((and (fboundp 'pipenv-project-p)
-                                  (pipenv-project-p))
-                             (list "pipenv" "run"))
-                            ((executable-find "pyenv") (list "pyenv" "version-name"))
-                            ((list (or doom-modeline-env-python-executable
-                                       python-shell-interpreter
-                                       "python")
-                                   "--version"))))
+                             (pipenv-project-p))
+                        (list "pipenv" "run"))
+                       ((executable-find "pyenv") (list "pyenv" "version-name"))
+                       ((list (or doom-modeline-env-python-executable
+                                  python-shell-interpreter
+                                  "python")
+                              "--version"))))
   :parser  (lambda (line) (let ((version (split-string line)))
-                            (if (>= (length version) 2)
-                                (cadr version)
-                              (car version)))))
+                       (if (>= (length version) 2)
+                           (cadr version)
+                         (car version)))))
 
 ;;;###autoload (autoload 'doom-modeline-env-setup-ruby "doom-modeline-env")
 (doom-modeline-def-env ruby
