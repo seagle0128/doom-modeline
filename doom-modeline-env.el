371ad23360e8b3cf16710cb13a85482938704746
Dev <g.devanandreddy@gmail.com>
2021-12-14 21:07:20 -0600

    Replaced iex with elixir, where iex doesn't quit gracefully leading to multiple stale beam processes (#493)
    Co-authored-by: Devanand Reddy <devanand.gangi.reddy@sap.com>

--- doom-modeline-env.el
+++ doom-modeline-env.el
@@ -258,7 +258,7 @@ PARSER should be a function for parsing COMMAND's output line-by-line, to
 ;;;###autoload (autoload 'doom-modeline-env-setup-elixir "doom-modeline-env")
 (doom-modeline-def-env elixir
   :hooks   'elixir-mode-hook
-  :command (lambda () (list (or doom-modeline-env-elixir-executable "iex") "--version"))
+  :command (lambda () (list (or doom-modeline-env-elixir-executable "elixir") "--version"))
   :parser  (lambda (line) (cadr (split-string line))))
 
 ;;;###autoload (autoload 'doom-modeline-env-setup-rust "doom-modeline-env")
