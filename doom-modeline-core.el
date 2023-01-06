4224fcee7a6356e312169ccdbb156f380298a843
Vincent Zhang <seagle0128@gmail.com>
2023-01-06 22:27:33 +0800

    Fix modified face.

--- doom-modeline-core.el
+++ doom-modeline-core.el
@@ -589,7 +589,7 @@ inactive windows."
   :group 'doom-modeline-faces)
 
 (defface doom-modeline-buffer-modified
-  '((t (:inherit (error bold) :background unspecified)))
+  '((t (:inherit (warning bold) :background unspecified)))
   "Face used for the \\='unsaved\\=' symbol in the mode-line."
   :group 'doom-modeline-faces)
 
