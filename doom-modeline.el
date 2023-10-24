125f235084c5b5922e0d0ead1b9c33d3c7a72347
Vincent Zhang <seagle0128@gmail.com>
2023-10-24 17:36:32 +0800

    chord: bump version

--- doom-modeline.el
+++ doom-modeline.el
@@ -4,8 +4,8 @@
 
 ;; Author: Vincent Zhang <seagle0128@gmail.com>
 ;; Homepage: https://github.com/seagle0128/doom-modeline
-;; Version: 4.0.2
-;; Package-Requires: ((emacs "25.1") (compat "29.1.4.2") (nerd-icons "0.0.1") (shrink-path "0.3.1"))
+;; Version: 4.1.0
+;; Package-Requires: ((emacs "25.1") (compat "29.1.4.2") (nerd-icons "0.1.0") (shrink-path "0.3.1"))
 ;; Keywords: faces mode-line
 
 ;; This file is not part of GNU Emacs.
