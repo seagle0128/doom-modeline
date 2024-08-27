36e32f09cc9bb28e8b4113a34c38de7ba25c2c9a
Vincent Zhang <seagle0128@gmail.com>
2024-08-27 23:18:15 +0800

    Revert "feat: display server information in help-echo"
    This reverts commit 54391c06270b0e8111206ca1df072e02ccc06951.

--- doom-modeline-segments.el
+++ doom-modeline-segments.el
@@ -142,8 +142,8 @@
 (declare-function edebug-help "edebug")
 (declare-function edebug-next-mode "edebug")
 (declare-function edebug-stop "edebug")
-(declare-function eglot--major-modes "ext:eglot")
-(declare-function eglot--server-info "ext:eglot" t t)
+(declare-function eglot "ext:eglot")
+(declare-function eglot--major-modes "ext:eglot" t t)
 (declare-function eglot-current-server "ext:eglot")
 (declare-function eglot-managed-p "ext:glot")
 (declare-function eglot-project-nickname "ext:eglot" t t)
@@ -2058,16 +2058,12 @@ mouse-1: Reload to start server")
                            ((and pending (cl-plusp pending)) 'doom-modeline-lsp-warning)
                            (nick 'doom-modeline-lsp-success)
                            (t 'doom-modeline-lsp-warning)))
-               (major-modes (and server (eglot--major-modes server)))
-               (server-info (and server (eglot--server-info server)))
-               (server-name (plist-get server-info :name))
-               (server-version (plist-get server-info :version))
                (icon (doom-modeline-lsp-icon eglot-menu-string face)))
           (propertize icon
-                      'help-echo (format "Eglot connected [%s]\n%s %s %s
+                      'help-echo (format "Eglot connected [%s]
 mouse-1: Display minor mode menu
 mouse-3: LSP server control menu"
-                                         nick server-name server-version major-modes)
+                                         nick)
                       'mouse-face 'doom-modeline-highlight
                       'local-map (let ((map (make-sparse-keymap)))
                                    (define-key map [mode-line mouse-1] eglot-menu)
